3532a5d162d1604fca009b2c6c755e0f5d84c287
maxsynnott <maxryansynnott@gmail.com>
2021-02-15 01:56:01 +0100

    ChessGame Component with run functionality

--- src/components/ChessGame.tsx
+++ src/components/ChessGame.tsx
@@ -1,7 +1,30 @@
+import axios from 'axios';
+import { useMutation, useQueryClient } from 'react-query';
+
 interface ChessGameProps {
-	game: any
+	game: any;
 }
 
 export default function ChessGame({ game }: ChessGameProps) {
-	return <li>{JSON.stringify(game, null, 4)}</li>
+	const queryClient = useQueryClient();
+	const url = 'http://localhost:8080/chess/' + game.id;
+
+	const { mutate: runGame } = useMutation(
+		() =>
+			axios.post(url + '/run', null, {
+				headers: {
+					Authorization: 'Bearer ' + localStorage.getItem('token'),
+				},
+			}),
+		{
+			onSuccess: () => queryClient.invalidateQueries(url),
+		},
+	);
+
+	return (
+		<div>
+			<button onClick={() => runGame()}>Run Game</button>
+			<pre>{JSON.stringify(game, null, 4)}</pre>
+		</div>
+	);
 }
